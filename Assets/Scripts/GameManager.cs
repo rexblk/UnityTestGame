fbb377b19b79539bb36afa8f482ee66d3d0135d1
Rex Black <RexNBlack@gmail.com>
2018-03-31 16:32:30 -0500

    added fall death and game manager

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour {
+    public static int CurrentScore;
+    public static int HighScore;
+
+    public static int CurrentLevel;
+    public static int UnlockedLevel;
+
+    private void Start()
+    {
+    }
+    internal static void CompleteLevel()
+    {
+        SceneManager.LoadScene(++CurrentLevel);
+    }
+}
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,29 +1,64 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerMovement : MonoBehaviour {
+public class PlayerMovement : MonoBehaviour
+{
     public float moveSpeed;
     private Vector3 input;
     public float maxSpeed = 10f;
     private Rigidbody Body;
+    public GameObject DeathParticles;
 
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start()
+    {
         print("imma cube yo");
         Body = GetComponent<Rigidbody>();
     }
-	
-	// Update is called once per frame
-	void Update () {
+
+    // Update is called once per frame
+    void Update()
+    {
         var body = GetComponent<Rigidbody>();
-        if(body.velocity.magnitude < maxSpeed)
+        if (body.velocity.magnitude < maxSpeed)
         {
             input = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
             Body.AddForce(input * moveSpeed);
         }
-        
+
+        if(Body.transform.position.y <= -2)
+            Die();
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        switch (collision.gameObject.tag)
+        {
+            case "Enemy":
+                Die();
+                break;
+            case "Goal":
+                GameManager.CompleteLevel();
+                break;
+
+
+        }
+    }
+
+    void Die()
+    {
+        Instantiate(DeathParticles, Body.transform.position, Quaternion.identity);
+        var spawn = Body.GetComponent<Spawn>();
+        if (spawn == null)
+        {
+            Body.position = new Vector3(0, 0.5f, 0);
+            return;
+        }
+        spawn.Respawn();
+
 
     }
 }
